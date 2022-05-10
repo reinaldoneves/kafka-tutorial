a1d0fe507c979cd2e13a75f18ed60cb2a46f0c1b
Reinaldo Neves <reinaldo_neves@hotmail.com>
2022-05-10 10:27:18 -0300

    Nelson is helping me out with Kafka. Salamaleico AmigosCode - Producer config

---
+++ src/main/java/kafkatutorial/kafkatutorial/KafkaApplication.java
@@ -0,0 +1,29 @@
+package kafkatutorial.kafkatutorial;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class KafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaApplication.class, args);
+	}
+
+	/***
+	 * This is a command line runner which will be executed after the application is started.
+	 * @param kafkaTemplate - The template to send the message to the kafka topic.
+	 * You need to tell to the template what topic you want to send the message to, and what message you want to send.
+	 *                      Here I'm sending a message to the topic "naldosTopic" with the message "Hello Kafka - Fora Bolsonaro".
+	 * @return
+	 */
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+		return args -> {
+			kafkaTemplate.send("naldosTopic", "Hello Kafka - Fora Bolsonaro");
+		};
+	}
+}
