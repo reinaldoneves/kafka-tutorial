265caa376815cf9fbb78ea9e3d5a2e1a75c3dc97
Reinaldo Neves <reinaldo_neves@hotmail.com>
2022-05-10 10:17:48 -0300

    Nelson is helping me out with Kafka. Salamaleico AmigosCode - Producer config

--- src/main/java/kafkatutorial/kafkatutorial/config/KafkaProducerConfig.java
+++ src/main/java/kafkatutorial/kafkatutorial/config/KafkaProducerConfig.java
@@ -3,17 +3,26 @@ package kafkatutorial.kafkatutorial.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/***
+ * Config class to the Kafka producer
+ * A producer is generally used to send messages to a topic
+ * the producer is in general a microservice.
+ * @author Reinaldo Neves
+ */
 public class KafkaProducerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    public Map<String, Object> producerConfigs() {
+    public Map<String, Object> producerConfig() {
         HashMap<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -26,8 +35,15 @@ public class KafkaProducerConfig {
      * at the value in the map. For now, it will be simply a String. (Maybe some day could be a Json?)
      * @return a ProducerFactory
      */
+    @Bean
     public ProducerFactory<String, String> producerFactory() {
-        return new org.springframework.kafka.core.DefaultKafkaProducerFactory<>(producerConfigs());
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(
+            ProducerFactory<String, String> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
     }
 
 }
